227ade7a3e5e3d0eb5736623ef8ea4868597cf17
nikiman8888 <nikiman88@yahoo.com>
2018-09-21 20:10:34 +0800

    Add files via upload

---
+++ Intro and Basic Syntax - Lab/11.MultiplicationTable2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.MultiplicationTable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num2 > 10)
+            {
+                Console.WriteLine("{0} X {1} = {2}", num1, num2, num1 * num2);
+                return;
+            }
+            for (int i = num2; i <= 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}", num1, i , num1 * i);
+            }
+        }
+    }
+}
---
+++ Intro and Basic Syntax - Lab/12.EvenNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            while (num%2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+
+                num = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("The number is: {0}",Math.Abs(num));
+
+        }
+    }
+}
