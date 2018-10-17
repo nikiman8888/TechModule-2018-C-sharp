def23e1d3f17dfd010582079f62f6fc64e3fd77d
nikiman8888 <nikiman88@yahoo.com>
2018-10-17 22:20:31 +0800

    Add files via upload

---
+++ Methods-Labs/07.MathPower/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            double number = double.Parse(Console.ReadLine());
+
+            int power = int.Parse(Console.ReadLine());
+
+            double result = GetResult(number, power);
+
+            Console.WriteLine(result);
+        }
+
+        private static double GetResult(double number, int power)
+        {
+            double result = Math.Pow(number, power);
+
+            return result;
+        }
+    }
+}
