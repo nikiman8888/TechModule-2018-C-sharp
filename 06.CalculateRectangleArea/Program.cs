84d8c69f0acb46ed2888e6ee329d23dfc559cc13
nikiman8888 <nikiman88@yahoo.com>
2018-10-17 21:27:43 +0800

    Add files via upload

---
+++ 06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double area = GetArea(width, height);
+
+            Console.WriteLine(area);
+        }
+
+        private static double GetArea(double width, double height)
+        {
+            double area = width * height;
+
+            return area;
+        }
+    }
+}
---
+++ 10.RepeatString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int count = int.Parse(Console.ReadLine());
+
+            PrintResult(input, count);
+        }
+
+        private static void PrintResult(string input, int count)
+        {
+            string temp = "";
+            for (int i = 0; i < count; i++)
+            {
+                temp += input; 
+            }
+            Console.WriteLine(temp);
+        }
+    }
+}
