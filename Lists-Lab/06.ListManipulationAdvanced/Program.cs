ed9df0eaeda20663070f253aae83a7799aeef748
nikiman8888 <nikiman88@yahoo.com>
2018-10-23 03:53:42 +0800

    Add files via upload

---
+++ Lists-Lab/06.ListManipulationAdvanced/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ListManipulationAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            List<double> inputData = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            bool change = false;
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split(' ');
+
+                string command = tokens[0];
+
+                switch (command)
+                {
+                    case "Add":
+
+                        int addNumber = int.Parse(tokens[1]);
+
+                        inputData.Add(addNumber);
+
+                        change = true;
+
+                        break;
+                    case "Remove":
+
+                        int numberToRemove = int.Parse(tokens[1]);
+
+                        inputData.Remove(numberToRemove);
+
+                        change = true;
+
+                        break;
+                    case "RemoveAt":
+
+                        int indexToRemove = int.Parse(tokens[1]);
+
+                        inputData.RemoveAt(indexToRemove);
+
+                        change = true;
+
+                        break;
+                    case "Insert":
+
+                        int numberToInsert = int.Parse(tokens[1]);
+
+                        int index = int.Parse(tokens[2]);
+
+                        inputData.Insert(index, numberToInsert);
+
+                        change = true;
+
+                        break;
+                    case "Contains":
+
+                        double containNumber = int.Parse(tokens[1]);
+                        if (inputData.Contains(containNumber))
+                        {
+                            Console.WriteLine("Yes");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No such number");
+                        }
+                        break;
+                    case "PrintEven":
+
+                        var tempEven = inputData.Where(x => x % 2 == 0).ToList();
+                        Console.WriteLine(string.Join(" ",tempEven));
+
+                        break;
+                    case "PrintOdd":
+
+                        var tempOdd = inputData.Where(x => x % 2 != 0).ToList();
+                        Console.WriteLine(string.Join(" ", tempOdd));
+
+                        break;
+                    case "GetSum":
+
+                        double allSum = inputData.Sum();
+                        Console.WriteLine(allSum);
+
+                        break;
+                    case "Filter":
+
+                        string sign = tokens[1];
+
+                        double numberLevel = int.Parse(tokens[2]);
+
+                        List<double> temp = new List<double>();
+                        switch (sign)
+                        {                          
+                            case ">":
+                                temp = inputData.Where(x => x > numberLevel).ToList();
+
+                                break;
+                            case "<":
+                                temp = inputData.Where(x => x < numberLevel).ToList();
+
+                                break;
+                            case ">=":
+
+                                temp = inputData.Where(x => x >= numberLevel).ToList();
+
+                                break;
+                            case "<=":
+
+                                temp = inputData.Where(x => x <= numberLevel).ToList();
+
+                                break;
+
+                        }
+                        Console.WriteLine(string.Join(" ",temp));
+
+                        break;
+                        
+                }
+                input = Console.ReadLine();
+            }
+            if (change)
+            {
+                Console.WriteLine(string.Join(" ",inputData));
+            }
+        }
+    }
+}
---
+++ Lists-Lab/07.MergingLists/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> firstList = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+
+            List<double> secondList = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+
+            var result = new List<double>();
+
+
+            int minLength = Math.Min(firstList.Count, secondList.Count);
+
+            if (firstList.Count == secondList.Count)
+            {
+                for (int i = 0; i < minLength; i++)
+                {
+                    result.Add(firstList[i]);
+                    result.Add(secondList[i]);
+                }                                
+            }
+            
+            else if (firstList.Count < secondList.Count)
+            {
+                var temp = secondList.Skip(minLength).ToList();
+
+                for (int i = 0; i < minLength; i++)
+                {
+                    result.Add(firstList[i]);
+                    result.Add(secondList[i]);
+                }
+                result.AddRange(temp);
+            }
+            else
+            {
+                var temp = firstList.Skip(minLength).ToList();
+
+                for (int i = 0; i < minLength; i++)
+                {
+                    result.Add(firstList[i]);
+                    result.Add(secondList[i]);
+                }
+                result.AddRange(temp);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
