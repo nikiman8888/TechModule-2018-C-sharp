29579c0897d02769ad464887f486be7cf682366b
nikiman8888 <nikiman88@yahoo.com>
2018-10-29 01:02:53 +0800

    Add files via upload

---
+++ Dictionaries-Lab/04.Largest-3-Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            input = input.OrderByDescending(x => x).Take(3).ToList();
+
+            Console.WriteLine(string.Join(" ",input));
+        }
+    }
+}
---
+++ Dictionaries-Lab/05.WordFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(' ').ToList();
+
+            input = input.Where(x => x.Length % 2 == 0).ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine,input));
+        }
+    }
+}
